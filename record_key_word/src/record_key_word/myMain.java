7ebfb6a0de15a080cefc7588e1648e2ef4becf5c
it21038914 <it21038914@my.sliit.lk>
2023-05-26 18:38:42 +0530

    record finished

--- record_key_word/src/record_key_word/myMain.java
+++ record_key_word/src/record_key_word/myMain.java
@@ -2,7 +2,25 @@ package record_key_word;
 
 public class myMain {
 	public static void main(String[] args) {
+		
+		Vehicle vehicle= new Vehicle("BMW", "MY343");
+		
+		  System.out.println(vehicle.brand());         // Accessing the brand using the generated getter method
+	      System.out.println(vehicle.licensePlate());  // Accessing the licensePlate using the generated getter method
+
+	      	//constructor generate automatically
+	      	//getter generate automatically
+	       //no setters are generate 
+	      	// because this records are immutable
+	      	
+	        System.out.println(vehicle.toString());
+		
+	        System.out.println(vehicle);
+		
 	
-		System.out.println("kavindu");
+	
+		
 }
+	
+	public record Vehicle(String brand,String licensePlate) {}
 }
